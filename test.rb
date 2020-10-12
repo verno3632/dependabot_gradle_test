18d9d29bc4889f172ee7d75ec9698344744700d0
verno3632 <verno3632@pixiv.com>
2020-10-12 16:50:51 +0900

    root

--- test.rb
+++ test.rb
@@ -6,7 +6,7 @@ require 'dependabot/source'
 target_repo_name = 'verno3632/dependabot_gradle_test'
 source = Dependabot::Source.new(provider: 'github', repo: target_repo_name)
 
-client = Octokit::Client.new access_token: "68b22bc8e2df0b9227706391212b2ed08601d035"
+client = Octokit::Client.new
 fetcher_class = Dependabot::Gradle::FileFetcher
 filenames = client.contents('verno3632/dependabot_gradle_test').map(&:name)
 
